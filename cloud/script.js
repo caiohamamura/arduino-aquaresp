47ebfe887d9d39da06880a53705c67655afbd1f6
caiohamamura <caiohamamura@gmail.com>
2023-01-24 03:20:23 -0300

    Working with firebase

--- cloud/script.js
+++ cloud/script.js
@@ -1,72 +1,60 @@
-// Set the authentication token as a cookie
-function setAuthCookie(token) {
-    document.cookie = "key=" + token + ";max-age=34560000;";
-}
+const firebaseConfig = {
+    apiKey: "AIzaSyD1fPoERfI700g03S_Omsk_RRZBliyDLso",
+    authDomain: "aquaresp.firebaseapp.com",
+    databaseURL: "https://aquaresp-default-rtdb.firebaseio.com",
+    projectId: "aquaresp",
+    storageBucket: "aquaresp.appspot.com",
+    messagingSenderId: "457840223205",
+    appId: "1:457840223205:web:77e24e08d750950501450c"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const database = firebase.database(app);
+const auth = firebase.auth(app);
 
 // Get the authentication token from the cookie
-function getAuthCookie() {
-    var name = "key=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) === " ") {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) === 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-var authToken = getAuthCookie();
 var authForm = document.getElementById("auth-form");
-var authInput = document.getElementById("auth-token-input");
+var userName = document.getElementById("user-name");
+var password = document.getElementById("password");
 var messageForm = document.getElementById("message-form");
-var socket;
+var signOut = document.getElementById("sign-out");
 
-if (authToken === "") {
+if (localStorage['username'] === undefined || localStorage['password'] === undefined) {
     authForm.style.display = "block";
+    signOut.style.diplay = 'none';
     authForm.addEventListener("submit", function (event) {
         event.preventDefault();
-        authToken = authInput.value;
-        setAuthCookie(authToken);
-        connectSocket();
+        localStorage['username'] = userName.value;
+        localStorage['password'] = password.value;
+        authFirebase();
     });
 } else {
-    connectSocket();
+    authFirebase();
 }
 
-function connectSocket() {
+let ref;
+
+async function authFirebase() {
     authForm.style.display = "none";
-    messageForm.style.display = "block";
-    socket = io();
-
-    // Send message on form submit
-    messageForm.addEventListener("submit", function (event) {
-        event.preventDefault();
-        var input = document.getElementById("message-input");
-        socket.emit("message", input.value);
-        input.value = "";
-    });
-
-    // Display received messages
-    var div = document.getElementById("received-messages");
-
-
-    socket.on("connect", function (message) {
-        console.log('conectado');
-    });
-
-    socket.on("message", data => {
-        console.log('msg');
-        div.innerHTML += "<br>" + data;
-
-    });
-
-    socket.on("disconnect", function (message) {
-        console.log('desconectado');
-    });
+    signOut.style.display = 'block';
+    await auth.signInWithEmailAndPassword(localStorage['username'], localStorage['password']);
+    ref = database.ref(`${auth.getUid()}/message`);
 }
 
+messageForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    var input = document.getElementById("message-input");
+    ref.set(input.value);
+    input.value = "";
+});
+
+async function signOut() 
+{
+    await auth.signOut();
+    signOut.style.diplay = 'none';
+    authForm.style.display = "block";
+    localStorage.removeItem('username');      
+    localStorage.removeItem('password');
+    userName.value = '';
+    password.value = '';
+}
