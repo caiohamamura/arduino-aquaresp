191808a86def0a2d00c45766f1387a9fe5bae770
caiohamamura <caiohamamura@gmail.com>
2023-01-24 03:59:12 -0300

    Add rest message

--- cloud/app.js
+++ cloud/app.js
@@ -1,3 +1,8 @@
+(async () => {
+const { initializeApp } = await import("firebase/app");
+const { getDatabase, ref, set } = await import("firebase/database");
+const { getAuth, signInWithEmailAndPassword } = await import("firebase/auth");
+
 const http = require('http');
 
 const PORT = process.env.PORT || 80;
@@ -17,6 +22,26 @@ app.get('/script.js', function (req, res, next) {
   res.sendFile(SCRIPT, { root: __dirname });
 });
 
+app.get('/grande/:port/:value', async function (req, res, next) {
+  const firebaseConfig = {
+    apiKey: "AIzaSyD1fPoERfI700g03S_Omsk_RRZBliyDLso",
+    authDomain: "aquaresp.firebaseapp.com",
+    databaseURL: "https://aquaresp-default-rtdb.firebaseio.com",
+    projectId: "aquaresp",
+    storageBucket: "aquaresp.appspot.com",
+    messagingSenderId: "457840223205",
+    appId: "1:457840223205:web:77e24e08d750950501450c"
+  };
+  const app = initializeApp(firebaseConfig); 
+  const database = getDatabase(app);
+  const auth = getAuth();
+  await signInWithEmailAndPassword(auth, process.env.user, process.env.password);
+  await set(ref(database, `${auth.currentUser.uid}/message`), `gpio${req.params.port} ${req.params.value}`);
+  auth.signOut();
+  res.send(200);
+});
+
 server.listen(PORT, () => {
   console.log(`Listening on *:${PORT}`);
 });
+})();
